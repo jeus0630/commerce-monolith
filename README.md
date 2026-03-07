3a71c914f12454358cc1fe005b6b181072a02f66
jewoo <jeus0630@gmail.com>
2026-03-07 20:04:02 +0900

    feat: initialize legacy commerce monolith project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Commerce Monolith Project
---
+++ src/main/java/com/jewoo/commerce/CommerceMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.jewoo.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceMonolithApplication.class, args);
+	}
+
+}
